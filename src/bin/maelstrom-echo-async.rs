e1d8375b064ab4ae986d49df4fa9619a239fd03b
Brandon Saint-John <bsaintjo@ucsc.edu>
2024-08-11 18:08:18 -0700

    tracing support

--- src/bin/maelstrom-echo-async.rs
+++ src/bin/maelstrom-echo-async.rs
@@ -1,9 +1,17 @@
-use std::{io::{self, BufRead, StdinLock}, ops::DerefMut, sync::Arc};
+use std::{
+    io::{self, BufRead, StdinLock},
+    ops::DerefMut,
+    sync::Arc,
+};
 
 use dist_sys::Message;
 use smol::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter}, lock::Mutex, Unblock
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
+    lock::Mutex,
+    Unblock,
 };
+use tracing::{info, instrument};
+use tracing_subscriber::FmtSubscriber;
 
 // fn run(stdin: &mut StdinLock) -> io::Result<()> {
 //     log::debug!("NEW MSG");
@@ -23,25 +31,52 @@ use smol::{
 
 async fn run() -> smol::io::Result<()> {
     let stdin = Arc::new(Mutex::new(BufReader::new(Unblock::new(io::stdin()))));
-    let mut stdout = BufWriter::new(Unblock::new(io::stdout()));
-    let x = smol::spawn(async move {
-        init(stdin.clone()).await
+    let stdout = Arc::new(Mutex::new(BufWriter::new(Unblock::new(io::stdout()))));
+    let x = smol::spawn({
+        let stdout = stdout.clone();
+        async move { init(stdin.clone(), stdout).await }
     });
-    let y = smol::spawn(async move {
-        stdout.write_all(b"test\n").await?;
-        stdout.flush().await
+    let y = smol::spawn({
+        let stdout = stdout.clone();
+        async move {
+            let mut stdout = stdout.lock_arc().await;
+            stdout.write_all(b"test\n").await?;
+            stdout.flush().await
+        }
     });
     smol::future::try_zip(x, y).await?;
     Ok(())
 }
 
-async fn init<R: AsyncBufReadExt + Unpin>(stdin: Arc<Mutex<R>>) -> smol::io::Result<()> {
-    let mut stdin = stdin.lock_arc().await;
+#[instrument(skip_all)]
+async fn init<R: AsyncBufReadExt + Unpin, W: AsyncWriteExt + Unpin>(
+    stdin: Arc<Mutex<R>>,
+    stdout: Arc<Mutex<W>>,
+) -> smol::io::Result<()> {
     let mut buf = String::new();
-    stdin.read_line(&mut buf).await?;
+    {
+        let mut stdin = stdin.lock_arc().await;
+        stdin.read_line(&mut buf).await?;
+    }
+    tracing::info!(buffer = buf);
+
+    let msg: Message = serde_json::from_str(&buf)?;
+    tracing::info!(message = ?msg);
+
+    let echo = msg.echo_ok();
+    tracing::info!(echo = ?echo);
+
+    let reply = serde_json::to_string(&echo)?;
+    tracing::info!(reply = reply);
+    {
+        let mut stdout = stdout.lock_arc().await;
+        stdout.write_all(reply.as_bytes());
+    }
     Ok(())
 }
 
 fn main() -> io::Result<()> {
+    let subscriber = FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
     smol::block_on(run())
 }
